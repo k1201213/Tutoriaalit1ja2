707c962414bd3401eaa58731f8b1c977a43558a8
k1201213 <manu.s.kortesmaki@seamk.fi>
2015-03-26 18:20:05 +0200

    eka versio

---
+++ ProductService/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ProductService.Models;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace ProductService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel()
+            );
+        }
+    }
+}
